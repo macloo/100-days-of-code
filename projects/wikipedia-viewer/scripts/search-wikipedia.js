39054ee5bbab742419cb27ead32d554dd8ce9522
macloo <mmcadams@gmail.com>
2017-01-14 22:46:12 -0500

    first files in Wikipedia viewer

---
+++ projects/wikipedia-viewer/scripts/search-wikipedia.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+$.ajax( {
+    url: remoteUrlWithOrigin,
+    data: queryData,
+    dataType: 'json',
+    type: 'POST',
+    headers: { 'Api-User-Agent': 'Example/1.0' },
+    success: function(data) {
+       // do something with data
+    }
+} );
+
+
+}); // end document ready
