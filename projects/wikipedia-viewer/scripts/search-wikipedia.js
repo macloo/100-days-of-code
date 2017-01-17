3cdd7397745db93b89fb802289dee8fbf7a2fd15
macloo <mmcadams@gmail.com>
2017-01-16 20:08:40 -0500

    updates to Wikipedia Viewer

--- projects/wikipedia-viewer/scripts/search-wikipedia.js
+++ projects/wikipedia-viewer/scripts/search-wikipedia.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
 
-
-$.ajax( {
-    url: remoteUrlWithOrigin,
-    data: queryData,
-    dataType: 'json',
-    type: 'POST',
-    headers: { 'Api-User-Agent': 'Example/1.0' },
-    success: function(data) {
-       // do something with data
+    function search() {
+        var q = $('#query').val();
+        console.log(q);
+        // clear input field
+        $('#query').val("");
     }
-} );
+
+    // listen for submit event
+    $('#search-form').submit(function(e) {
+        e.preventDefault();
+        search();
+    });
+
+    /*
+    $.ajax( {
+        url: "https://en.wikipedia.org/w/api.php",
+        data: queryData,
+        dataType: 'json',
+        type: 'POST',
+        headers: { 'Api-User-Agent': 'Example/1.0' },
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+        },
+        error: function (errorMessage) {
+        }
+    } );
+    */
 
 
 }); // end document ready
