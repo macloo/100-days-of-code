1e300a14d4e6178df667a0eb533ed131f2d9582a
macloo <mmcadams@gmail.com>
2017-01-18 21:39:47 -0500

    update search string

--- projects/wikipedia-viewer/scripts/search-wikipedia.js
+++ projects/wikipedia-viewer/scripts/search-wikipedia.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
 
-    function search() {
-        var q = $('#query').val();
-        console.log(q);
-        // clear input field
-        $('#query').val("");
-    }
-
     // listen for submit event
     $('#search-form').submit(function(e) {
         e.preventDefault();
         search();
     });
 
-    /*
-    $.ajax( {
-        url: "https://en.wikipedia.org/w/api.php",
-        data: queryData,
-        dataType: 'json',
-        type: 'POST',
-        headers: { 'Api-User-Agent': 'Example/1.0' },
-        success: function (data, textStatus, jqXHR) {
-            console.log(data);
-        },
-        error: function (errorMessage) {
-        }
-    } );
+    function search() {
+        // get text from input field
+        var q = $('#query').val();
+        // clear input field
+        $('#query').val("");
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+        q;
+        console.log(url);
+
+        /* $.ajax( {
+            url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+            // data: queryData,
+            dataType: 'json',
+            type: 'POST',
+            headers: { 'Api-User-Agent': 'Example/1.0' },
+            success: function (data, textStatus, jqXHR) {
+                console.log(data);
+            },
+            error: function (errorMessage) {
+            }
+        } );
     */
+    }
 
 
 }); // end document ready
