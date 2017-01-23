a4223e2f4b4edcfa11797f2b3594da6b2a6342b2
macloo <mmcadams@gmail.com>
2017-01-23 13:46:44 -0500

    Ajax call works
    I’m using action=opensearch here. Will try action=query next.

--- projects/wikipedia-viewer/scripts/search-wikipedia.js
+++ projects/wikipedia-viewer/scripts/search-wikipedia.js
@@ -13,21 +13,24 @@ $(document).ready(function() {
         $('#query').val("");
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
         q;
-        console.log(url);
+        // console.log(url);
 
-        /* $.ajax( {
-            url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        $.ajax( {
+            type: 'POST',
+            url: url + "&callback=?",
             // data: queryData,
             dataType: 'json',
-            type: 'POST',
             headers: { 'Api-User-Agent': 'Example/1.0' },
             success: function (data, textStatus, jqXHR) {
-                console.log(data);
+                for (var i=0; i < data[1].length; i++) {
+                    console.log(data[1][i] + " " + data[2][i] + " " +
+                    data[3][i] + "\n");
+                }
             },
             error: function (errorMessage) {
             }
         } );
-    */
+
     }
 
 
