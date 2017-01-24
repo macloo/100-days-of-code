6bd4ebb2781358afeed3a9e3efe507fe846814d7
macloo <mmcadams@gmail.com>
2017-01-23 19:38:33 -0500

    now gets page ID and title
    These search results are way, way better than previous

--- projects/wikipedia-viewer/scripts/search-wikipedia.js
+++ projects/wikipedia-viewer/scripts/search-wikipedia.js
@@ -11,25 +11,24 @@ $(document).ready(function() {
         var q = $('#query').val();
         // clear input field
         $('#query').val("");
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+        // api query
+        var url =  "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" +
         q;
-        // console.log(url);
 
-        $.ajax( {
+        $.ajax({
             type: 'POST',
-            url: url + "&callback=?",
-            // data: queryData,
-            dataType: 'json',
+            url: url,
+            dataType: 'jsonp',
             headers: { 'Api-User-Agent': 'Example/1.0' },
             success: function (data, textStatus, jqXHR) {
-                for (var i=0; i < data[1].length; i++) {
-                    console.log(data[1][i] + " " + data[2][i] + " " +
-                    data[3][i] + "\n");
+                var list = data.query.pages;
+                for (var key in list) {
+                    if (list.hasOwnProperty(key)) {
+                        console.log( key + ": " + list[key].title );
+                    }
                 }
-            },
-            error: function (errorMessage) {
-            }
-        } );
+            } // end success
+        });
 
     }
 
